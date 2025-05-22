09dc12fd1c23bee541d25d2b2e4853c6d72181fb
SONG YINRUI <78004370+RickSong218@users.noreply.github.com>
2025-05-22 22:03:27 +0800

    Used DTOs (AddCartRequest, UpdateCartRequest) for request bodies

--- src/main/java/com/iss/shoppingcart/dto/AddCartRequest.java
+++ src/main/java/com/iss/shoppingcart/dto/AddCartRequest.java
@@ -1,4 +1,17 @@
 package com.iss.shoppingcart.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/*
+ 添加购物车请求体 DTO
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AddCartRequest {
+    private Long userId;
+    private Long productId;
+    private Integer quantity;
 }
--- src/main/java/com/iss/shoppingcart/dto/UpdateCartRequest.java
+++ src/main/java/com/iss/shoppingcart/dto/UpdateCartRequest.java
@@ -1,4 +1,17 @@
 package com.iss.shoppingcart.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/*
+ 更新购物车商品数量请求体 DTO
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpdateCartRequest {
+    private Long userId;
+    private Long productId;
+    private Integer quantity;
 }
